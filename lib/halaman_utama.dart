afcb2de56145ed373463ea4d22f236be40c63c96
Muhammad Husain Al Mubarak <husainalmubarak25@gmail.com>
2026-08-28 15:50:34 +0800

    membuat halaman_utama.dart, merapikan main.dart dan widget_test.dart

---
+++ lib/halaman_utama.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'data_kontak.dart';
+
+class HalamanUtama extends StatefulWidget {
+  @override
+  State<HalamanUtama> createState() => _HalamanUtamaState();
+}
+
+class _HalamanUtamaState extends State<HalamanUtama> {
+  List<DataKontak> daftarKontak = [];
+  bool isLoading = false;
+
+   @override
+  void initState() {
+    super.initState();
+    ambilDataDariInternet(); 
+  }
+
+  Future<void> ambilDataDariInternet() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      var url = Uri.parse('https://reqres.in/api/users?page=1');
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        List<dynamic> dataJsonMentah = jsonDecode(response.body)['data'];
+
+        setState(() {
+          daftarKontak = dataJsonMentah.map((item) => DataKontak.fromJson(item)).toList();
+        });
+      }
+    } catch (e) {
+      print('Error: $e');
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Aplikasi Kontak", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),), backgroundColor: Colors.blue,),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child:Column(
+          children: [
+            Text("Daftar Kontak", style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),),
+            
+            SizedBox(height: 20),
+
+            Expanded(
+              child: isLoading ? Center(child: CircularProgressIndicator()) : ListView.builder(
+                itemCount: daftarKontak.length,
+                itemBuilder: (context, index) {
+
+                  DataKontak kontak = daftarKontak[index];
+
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.blue[100],
+                        backgroundImage: NetworkImage(kontak.avatar),
+                      ),
+                      title: Text(kontak.namaDepan),
+                      subtitle: Text(kontak.email),
+                      
+                      // onTap: () {
+                      //   Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //       builder: (context) => DetailKontak(kontak: kontak),
+                      //     ),
+                      //   );
+                      // }
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
